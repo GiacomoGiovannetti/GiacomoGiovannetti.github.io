845d3e40f4fad2a5e9c8cadfb72250e2d0bc5970
Giacomo Giovannetti <giovannettii.giacomo@gmail.com>
2022-11-01 18:12:04 +0100

    Conversione a Sass

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+function toggle(){
+    var x = document.getElementById("links");
+    if(x.className=== "links"){
+        x.className += "responsive";
+    } else{
+        x.className="links";
+    }
+}
